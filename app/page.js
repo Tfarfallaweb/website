2d50a2a0de26de70dd8e99294da31277d4ef81f0
Waleed Nasir <107930810+WaleedNasirAlsiraj@users.noreply.github.com>
2024-03-26 21:48:29 +0500

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HomeComp from "@/components/HomeComp"
+import Images from '@/components/Images'
+
+const Home = () => {
+
+  
+
+  return (
+    <div>
+      <HomeComp/>
+      
+      <Images/>
+      
+    </div>
+  )
+}
+
+export default Home
